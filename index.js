2e1ba59b0e07a419739711e609f0d4391b8b456d
Victor Franco <francovictor.dev@gmail.com>
2022-10-18 14:04:01 -0300

    feat: enable country selection

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+const countrySelection = document.querySelector('#country-select');
+
 const year = '2016';
-const url = `https://servicodados.ibge.gov.br/api/v1/paises/BR/indicadores?periodo=${year}`;
+
+var country = countrySelection.options[countrySelection.selectedIndex].value;
+
+
+countrySelection.addEventListener('change', (event) => {
+   console.log(countrySelection.options[countrySelection.selectedIndex].value);
+   country = event.target.value;
+   console.log(country);
+   
+} )
+
+console.log(country);
+
+const url = `https://servicodados.ibge.gov.br/api/v1/paises/${country}/indicadores?periodo=${year}`;
+
 const searchResults = document.querySelector('#search-results');
 
+
+
+
 async function getObj() {
+   console.log(url);
    const response = await fetch(url);
 
    const data = await response.json();
@@ -10,17 +30,18 @@ async function getObj() {
 
    data.map((item) => {
       try {
+         console.log(country);
          const div = document.createElement('div');
-      const title = document.createElement('h3');
-      const number = document.createElement('p');
+         const title = document.createElement('h3');
+         const number = document.createElement('p');
 
-      title.innerText = item.indicador;
-      number.innerHTML = `${item.series[0].serie[0][year]* item.unidade.multiplicador} ${item.unidade.id}`
+         title.innerText = item.indicador;
+         number.innerHTML = `${item.series[0].serie[0][year]* item.unidade.multiplicador} ${item.unidade.id}`
 
-      div.appendChild(title);
-      div.appendChild(number);
+         div.appendChild(title);
+         div.appendChild(number);
 
-      searchResults.appendChild(div);
+         searchResults.appendChild(div);
          
       } catch (error) {
          console.log(error);
@@ -32,5 +53,3 @@ async function getObj() {
 
    
 }
-
-// getObj();
