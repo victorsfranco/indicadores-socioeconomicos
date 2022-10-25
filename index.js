07df7e42255ebde1fc4996ffd24f9e9b197a5ccb
Victor Franco <francovictor.dev@gmail.com>
2022-10-25 14:02:11 -0300

    feat: agrouping requests queue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let country = document.querySelector('#country-select').value;
 
 document.querySelector('#country-select').addEventListener('change', (event) => {
-   country = event.target.value;  
+   country = event.target.value;
 } )
 
 let year = document.querySelector('#year-select').value;
@@ -14,6 +14,21 @@ document.querySelector('#year-select').addEventListener('change', (event) => {
 const searchResults = document.querySelector('#search-results');
 
 async function getObj() {
+
+   const allResults = document.createElement('div');
+   allResults.setAttribute('id', 'all-results');
+   searchResults.appendChild(allResults);
+
+   const resultHeader = document.createElement('header');
+   resultHeader.setAttribute('class', 'result-header');
+   allResults.appendChild(resultHeader);
+
+   const resultTitle = document.createElement('h3');
+   resultHeader.setAttribute('class', 'result-title');
+   resultHeader.appendChild(resultTitle);
+
+   resultTitle.innerText = 'countryName - Year';
+
    const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/paises/${country}/indicadores?periodo=${year}`);
 
    const data = await response.json();
@@ -22,20 +37,20 @@ async function getObj() {
       try {
          
          const div = document.createElement('div');
-         const title = document.createElement('h3');
+         const indicatorTitle = document.createElement('h3');
          const indicatorValue = document.createElement('p');
 
          div.setAttribute('class', 'indicator-box');
-         title.setAttribute('class', 'indicator-title');
+         indicatorTitle.setAttribute('class', 'indicator-title');
          indicatorValue.setAttribute('class', 'indicator-value');
 
-         title.innerText = item.indicador;
+         indicatorTitle.innerText = item.indicador;
          indicatorValue.innerHTML = `${item.series[0].serie[0][year] * item.unidade.multiplicador} ${item.unidade.id}`
 
-         div.appendChild(title);
+         div.appendChild(indicatorTitle);
          div.appendChild(indicatorValue);
 
-         searchResults.appendChild(div);
+         allResults.appendChild(div);
          
       }
       catch (error) {
